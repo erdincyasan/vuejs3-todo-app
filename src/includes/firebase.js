6ece8dea8e45ff23daccb270c0c0cae3f482c389
erdincyasan <81456476+erdincyasan@users.noreply.github.com>
2021-08-13 15:27:52 +0300

    Update firebase.js

--- src/includes/firebase.js
+++ src/includes/firebase.js
@@ -1,12 +1,7 @@
 import firebase from "firebase/app";
 import "firebase/firestore";
 const config = {
-  apiKey: "AIzaSyADXHTBFP7jUMI7apfBTKD9YPEod51tlSA",
-  authDomain: "todo-app-aeb25.firebaseapp.com",
-  projectId: "todo-app-aeb25",
-  storageBucket: "todo-app-aeb25.appspot.com",
-  messagingSenderId: "864577890664",
-  appId: "1:864577890664:web:85d2397748620899310edd",
+ //firebase-configs
 };
 firebase.initializeApp(config);
 const db = firebase.firestore();
